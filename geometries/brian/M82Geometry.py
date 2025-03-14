f0c285248bcf2ac53fd39f8ffd8bca171562e58d
Brian Carcich <briantcarcich@gmail.com>
2025-03-14 13:33:59 -0400

    Reduced brian/ subdirectory to M82 data only

--- geometries/brian/M82Geometry.py
+++ geometries/brian/M82Geometry.py
@@ -2,29 +2,25 @@ import spiceypy as sp
 
 def main():
 
-    mk = '../kernels/asperaMetaKernel.tm'
-    spk = 'NewM82.bsp'
-    pck = 'NewM82.tpc'
+    mk = '../kernels/mk/asperaMetaKernel.tm'
+    spk = 'M82spk.bsp'
 
     sp.furnsh(mk)
     sp.furnsh(spk)
-    sp.furnsh(pck)
 
     targ = 9999000
-    utc = '2025 Jan 01 00:00:00'
+    utc = '2025 Jun 01 00:00:01'
     et = sp.str2et(utc)
     ref = 'J2000'
     abcorr = 'NONE'
     obs = 399 # 'EARTH'
 
     [ptarg, lt] = sp.spkezp(targ, et, ref, abcorr, obs)
-    # [ptarg, lt] = sp.spkpos(targ, et, ref, abcorr, obs)
 
     print(ptarg)
     print(lt)
 
     sp.unload(mk)
     sp.unload(spk)
-    sp.unload(pck)
 
 main()
